91e358fa0d124bf27a33c69c2dc7f4c5e5aece25
STUDENT Adrian Palfi <Adrian.Palfi@students.ittralee.ie>
2023-09-27 13:05:09 +0100

    Signed-off-by: STUDENT Adrian Palfi <Adrian.Palfi@students.ittralee.ie>

--- MyRADProject/Assets/cubecontrolscript.cs
+++ MyRADProject/Assets/cubecontrolscript.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class cubecontrolscript : MonoBehaviour
 {
+    Rigidbody myRB;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 7, 0);
+     myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) )
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * Time.deltaTime;
+
+            myRB.AddForce(transform.forward);
+
+            //transform.position += transform.forward * Time.deltaTime;
         }
-        
-        if (Input.GetKey(KeyCode.A))
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.Rotate(Vector3.up,-90 * Time.deltaTime); 
+
+            myRB.AddExplosionForce(10, transform.position + Vector3.down, 5);
+
+            //transform.position += transform.forward * Time.deltaTime;
+        }
+         
+        
+            if (Input.GetKey(KeyCode.A))
+        {    
+           
+            transform.Rotate(Vector3.up, -90 * Time.deltaTime);
+            
         }
     }
 }
